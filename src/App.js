3c4a4c16921808672f9002550097b1cc8cdfefac
Rishad05 <iu.rishad5@gmail.com>
2021-07-26 17:09:37 +0600

    3rd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Component/FrontEnd/Home/Header/Navbar/Navbar';
+import Slider from './Component/FrontEnd/Home/Header/Slider/Slider';
 
 function App() {
   return (
+
     <div className="App">
+      <Navbar></Navbar>
+      <Slider></Slider>
       <h1>Hello Mysterious!</h1>
     </div>
   );
