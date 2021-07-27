4a9ad24887e5a9c37cb406dc891a93e712c67b51
dalwer001 <dalwer.official@gmail.com>
2021-07-27 17:17:20 +0600

    remove card design

--- src/Component/FrontEnd/SingleProduct/SingleProductDetails/SingleProductsDetails.js
+++ src/Component/FrontEnd/SingleProduct/SingleProductDetails/SingleProductsDetails.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import './SingleProductsDetails.css';
 
 const SingleProductsDetails = () => {
+    const [singleProduct, setSingleProduct] = useState([]);
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        const singleProduct = async () => {
+            const res = await axios.get(`/products/${id}`);
+            setSingleProduct(res.data);
+            console.log(res.data);
+        }
+        singleProduct();
+    }, [id])
+
+    const { title, price, image, category, description } = singleProduct;
+
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <section className="container">
+            <div className="row d-flex align-items-center">
+                <div className="col-md-6 P-5">
+                    <img src={image} alt="" className="w-75 " />
+                </div>
+                <div className="col-md-6">
+                    <div>
+                        <h1>{title}</h1>
+                        <h6>${price}</h6>
+                        <p>{description}</p>
+                    </div>
+
+                    <div className="d-flex align-items-center">
+                        <div>
+                            <p className="fs-4 single-size-text mt-2">Size</p>
+                        </div>
+                        <div className=" mx-5 d-flex single-size-bg rounded-pill p-2">
+                            <a href="#" alt="" className=" product-size-hover mx-3 text-decoration-none text-dark">XS</a>
+                            <a  href="#" alt=""  className="mx-3 text-decoration-none text-dark">S</a>
+                            <a  href="#" alt="" className="mx-3 text-decoration-none text-dark">M</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
     );
 };
 
