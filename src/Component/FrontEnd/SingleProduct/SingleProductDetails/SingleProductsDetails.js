65591ee7fa04ff3d2eaf2959aa1b8f650ff57e79
dalwer001 <dalwer.official@gmail.com>
2021-07-27 15:13:43 +0600

    single product page and home page recent product section error correction

---
+++ src/Component/FrontEnd/SingleProduct/SingleProductDetails/SingleProductsDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleProductsDetails = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default SingleProductsDetails;
