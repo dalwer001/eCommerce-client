7e6c31851ad1c8e06f9f7f868ba2cc5ebdac82fa
dalwer001 <dalwer.official@gmail.com>
2021-07-28 15:51:12 +0600

    load products more

--- src/Component/FrontEnd/Home/Products/Products.js
+++ src/Component/FrontEnd/Home/Products/Products.js
@@ -2,10 +2,16 @@ import axios from 'axios';
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import ProductDetails from '../ProductDetails/ProductDetails';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import Bounce from 'react-reveal/Bounce';
+import loadingProduct from '../../../../Images/load-product.gif';
 import './Product.css';
 
 const Products = () => {
     const [recentProducts, setRecentProducts] = useState([]);
+    const [visibleProduct, setVisibleProduct] = useState(9);
+    // const [spinner, setSpinner] = useState(false);
 
     useEffect(() => {
         const productsLoaders = async () => {
@@ -16,7 +22,14 @@ const Products = () => {
     }, []);
 
     //product slicing
-    const productLoader = recentProducts.slice(0,9);
+    const productLoader = recentProducts.slice(0, visibleProduct);
+
+    // load product more
+    const showMoreProducts = () => {
+        setVisibleProduct((preValue) => preValue + 6);
+        if(setVisibleProduct)
+        // setSpinner(true);
+    }
 
     // cart product add
     const [cartProducts, setCartProducts] = useState([]);
@@ -40,8 +53,21 @@ const Products = () => {
                             />)
                     }
                 </div>
+
+                {/* lOAD MORE PRODUCT */}
+                <Bounce left cascade>
+                    <div className="d-flex justify-content-center mt-5">
+                        <button className="product-load-btn rounded-pill" onClick={showMoreProducts}>
+                            <div className="d-flex justify-content-center align-items-center mt-2">
+                                <p>Load More</p>
+                                <p className="mx-2"><FontAwesomeIcon size="1x" className=" text-dark ms-0" icon={faArrowRight} /></p>
+                            </div>
+                        </button>
+                    </div>
+                </Bounce>
+
             </div>
-        </div>
+        </div >
     );
 };
 
