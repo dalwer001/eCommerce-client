01ac10c708ad8a1b8ccd273144b7b42a2d708a77
naimur4062 <naimur4062@gmail.com>
2021-07-29 01:03:59 +0800

    update

--- src/Component/FrontEnd/Home/Cart/Cart.js
+++ src/Component/FrontEnd/Home/Cart/Cart.js
@@ -1,15 +1,52 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../../../../App';
+import './Cart.css';
 
 const Cart = () => {
     const [cartProducts, setCartProducts] = useContext(CartContext);
-    console.log('id', cartProducts)
 
     return (
-        <div>
-            {
-                cartProducts.map(p => <p>{p.title}</p>)
-            }
+        <div className="container">
+            <div className="row title d-flex justify-content-between text-center mt-5">
+                <h6 className="col-md-2">Image</h6>
+                <h6 className="col-md-2">Product Name</h6>
+                <h6 className="col-md-2">Quantity</h6>
+                <h6 className="col-md-2">Unit Price</h6>
+                <h6 className="col-md-2">Total</h6>
+            </div>
+            <div className="row d-flex justify-content-between text-center mt-2 description">
+                <div className="col-md-2 image">
+                    {
+                        cartProducts.map(p => <p><img src={p.image} alt="..." /></p>)
+                    }
+                </div>
+                <div className="col-md-2">
+                    {
+                        cartProducts.map(p => <p>{p.title}</p>)
+                    }
+                </div>
+                <div className="col-md-2">
+                    {
+                        cartProducts.map(p => <p>{p.quantity}</p>)
+                    }
+                </div>
+                <div className="col-md-2">
+                    {
+                        cartProducts.map(p => <p>${p.price}</p>)
+                    }
+                </div>
+                <div className="col-md-2">
+                    {
+                        cartProducts.map(p => <p>${p.price * p.quantity}</p>)
+                    }
+                </div>
+            </div>
+            <div className="calculation d-flex justify-content-end mt-2">
+                <div>
+                    <h5>Sub-Total: $500</h5>
+                    <h4>Total: $600</h4>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Component/FrontEnd/Shared/Navbar/Navbar.js
+++ src/Component/FrontEnd/Shared/Navbar/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/home">Home</a>
+              <Link to="/home" className="nav-link active" aria-current="page" >Home</Link>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/shop">Shop</a>
