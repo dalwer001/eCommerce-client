336ab0601a4feb27c975a8f66b653f55c90222d2
naimur4062 <naimur4062@gmail.com>
2021-07-29 12:31:32 +0800

    update

--- src/Component/FrontEnd/Home/Cart/Cart.js
+++ src/Component/FrontEnd/Home/Cart/Cart.js
@@ -1,50 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { CartContext } from '../../../../App';
 import './Cart.css';
+import CartDetails from './CartDetails';
 
 const Cart = () => {
     const [cartProducts, setCartProducts] = useContext(CartContext);
+    const [singleProductTotal, setSingleProductTotal] = useState(0);
+    cartProducts.forEach(pd => {
+        let singleProductTotal = 0;
+        const singleProductPrice = pd.price * pd.quantity;
+        singleProductTotal += singleProductPrice;
+        console.log(singleProductTotal)
+        // setSingleProductTotal(singleProductTotal);
+    });
+
 
     return (
         <div className="container">
-            <div className="row title d-flex justify-content-between text-center mt-5">
-                <h6 className="col-md-2">Image</h6>
-                <h6 className="col-md-2">Product Name</h6>
-                <h6 className="col-md-2">Quantity</h6>
-                <h6 className="col-md-2">Unit Price</h6>
-                <h6 className="col-md-2">Total</h6>
+            <div className="row title d-flex justify-content-between text-start mt-5">
+                <h6 className="col-md-1 col-sm-1 col-1">Image</h6>
+                <h6 className="col-md-6 col-sm-6 col-6">Product Name</h6>
+                <h6 className="col-md-2 col-sm-2 col-2">Quantity</h6>
+                <h6 className="col-md-2 col-sm-2 col-2">Unit Price</h6>
+                <h6 className="col-md-1 col-sm-1 col-1">Total</h6>
             </div>
-            <div className="row d-flex justify-content-between text-center mt-2 description">
-                <div className="col-md-2 image">
-                    {
-                        cartProducts.map(p => <p><img src={p.image} alt="..." /></p>)
-                    }
-                </div>
-                <div className="col-md-2">
-                    {
-                        cartProducts.map(p => <p>{p.title}</p>)
-                    }
-                </div>
-                <div className="col-md-2">
-                    {
-                        cartProducts.map(p => <p>{p.quantity}</p>)
-                    }
-                </div>
-                <div className="col-md-2">
-                    {
-                        cartProducts.map(p => <p>${p.price}</p>)
-                    }
-                </div>
-                <div className="col-md-2">
-                    {
-                        cartProducts.map(p => <p>${p.price * p.quantity}</p>)
-                    }
-                </div>
-            </div>
-            <div className="calculation d-flex justify-content-end mt-2">
-                <div>
-                    <h5>Sub-Total: $500</h5>
-                    <h4>Total: $600</h4>
+            {
+                cartProducts.map(pd => <CartDetails key={pd.id} pd={pd} />)
+            }
+            <div className="d-flex justify-content-end mt-2 calculation-section">
+                <div className="calculation">
+                    <h5>Sub-Total: <span className="money">${ }</span></h5>
+                    <h4>Total: <span className="money">$</span></h4>
                 </div>
             </div>
         </div>
---
+++ src/Component/FrontEnd/Home/Cart/CartDetails.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const CartDetails = ({ pd }) => {
+    const { image, title, quantity, price } = pd;
+    return (
+        <div>
+            <div className="row d-flex justify-content-between text-start mt-2 description">
+                <div className="col-md-1 col-sm-1 col-1 image">
+                    <img src={image} alt="" />
+                </div>
+                <div className="col-md-6 col-sm-6 col-6">
+                    <p>{title}</p>
+                </div>
+                <div className="col-md-2 col-sm-2 col-2 quantity-input">
+                    <input type="number" id="tentacles" name="tentacles" />
+                </div>
+                <div className="col-md-2 col-sm-2 col-2">
+                    <p>${price}</p>
+                </div>
+                <div className="col-md-1 col-sm-1 col-1">
+                    <p>${quantity * price}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CartDetails;
