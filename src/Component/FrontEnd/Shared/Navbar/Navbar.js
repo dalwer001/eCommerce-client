b25e3ea163fa7123e8ded285b808041783885dad
Rishad05 <iu.rishad5@gmail.com>
2021-07-30 12:03:49 +0600

    build the search component

--- src/Component/FrontEnd/Shared/Navbar/Navbar.js
+++ src/Component/FrontEnd/Shared/Navbar/Navbar.js
@@ -1,24 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartPlus, faSearch, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faCartPlus, faHeart } from '@fortawesome/free-solid-svg-icons';
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../../Images/logo.jpg';
 import './Navbar.css';
 import { CartContext } from '../../../../App';
-import { Collapse } from 'react-bootstrap';
-import { TextField} from '@material-ui/core';
+import SearchProduct from '../../Home/SearchProduct/SearchProduct';
+
+
 
 const Navbar = () => {
-  const [showSearchBox, setShowSearchBox] = useState(false);
-  const [data, setData] = useState([]);
-
-async function search(key) {
-  console.log(key);
-  let result = await fetch ("https://fakestoreapi.com/products"+ key);
-  console.log(result)
-  result = await result.json();
-  setData(result)
-}
+ 
+  
   const [cartProducts, setCartProducts] = useContext(CartContext);
   let cartProductQuantity = 0;
   cartProducts.forEach(p => {
@@ -60,10 +53,7 @@ async function search(key) {
 
             {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> */}
           
-            <Link onClick={() => setShowSearchBox(!showSearchBox)} to="#"><FontAwesomeIcon size="2x" className="search ms-0 m-2 " icon={faSearch} /></Link>
-            <Collapse in = {showSearchBox}>
-            <TextField onChange ={(e) => search(e.target.value)} name="title" label="Search" fullWidth  />
-            </Collapse>
+           <SearchProduct></SearchProduct>
             <Link to="#"><FontAwesomeIcon size="2x" className=" wishlistNav  ms-0 m-2" icon={faHeart} /></Link>
             <Link to="/cart">
               <button type="button" className="btn btn-sm btn-light position-relative p-0 m-0">
