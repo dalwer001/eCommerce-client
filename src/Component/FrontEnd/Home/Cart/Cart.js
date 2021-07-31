587d15c4c7b6f0cf262b1c74be493b5e6192276f
naimur4062 <naimur4062@gmail.com>
2021-07-31 22:20:26 +0800

    update

--- src/Component/FrontEnd/Home/Cart/Cart.js
+++ src/Component/FrontEnd/Home/Cart/Cart.js
@@ -1,7 +1,9 @@
+import { Button } from 'react-bootstrap';
 import React, { useContext } from 'react';
 import { CartContext, TotalContext } from '../../../../App';
 import './Cart.css';
 import CartDetails from './CartDetails';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
     const [cartProducts, setCartProducts] = useContext(CartContext);
@@ -9,22 +11,35 @@ const Cart = () => {
 
     return (
         <div className="container">
-            <div className="row title d-flex justify-content-between text-start mt-5">
-                <h6 className="col-md-1 image">Image</h6>
-                <h6 className="col-md-6 col-sm-6 col-6">Product Name</h6>
-                <h6 className="col-md-2 col-sm-3 col-3 text-center">Quantity</h6>
-                <h6 className="col-md-2 unitPrice">Unit Price</h6>
-                <h6 className="col-md-1 col-sm-3 col-3 text-center">Total</h6>
-            </div>
+            <h3 className="mt-5 text-center">Shopping Cart</h3>
             {
-                cartProducts.map(pd => <CartDetails key={pd.id} pd={pd} />)
+                cartProducts.length ?
+                    <>
+                        <div className="row title d-flex justify-content-between text-start mt-3">
+                            <h6 className="col-md-1 image">Image</h6>
+                            <h6 className="col-md-6 col-sm-6 col-6">Product Name</h6>
+                            <h6 className="col-md-2 col-sm-3 col-3 text-center">Quantity</h6>
+                            <h6 className="col-md-2 unitPrice">Unit Price</h6>
+                            <h6 className="col-md-1 col-sm-3 col-3 text-center">Total</h6>
+                        </div>
+                        {
+                            cartProducts.map(pd => <CartDetails key={pd.id} pd={pd} />)
+                        }
+                        <div className="d-flex justify-content-end mt-2 calculation-section">
+                            <div className="calculation mt-2">
+                                <h5>Sub-Total: <span className="money">${grandTotal.toFixed(2)}</span></h5>
+                                <h4>Total: <span className="money">${grandTotal.toFixed(2)}</span></h4>
+                            </div>
+                        </div>
+                    </>
+                    :
+                    <div className="text-center">
+                        <p className="mt-3">Your shopping cart is empty!</p>
+                        <Link to="/home">
+                            <Button variant="primary">Continue</Button>
+                        </Link>
+                    </div>
             }
-            <div className="d-flex justify-content-end mt-2 calculation-section">
-                <div className="calculation mt-2">
-                    <h5>Sub-Total: <span className="money">${grandTotal.toFixed(2)}</span></h5>
-                    <h4>Total: <span className="money">${grandTotal.toFixed(2)}</span></h4>
-                </div>
-            </div>
         </div>
     );
 };
