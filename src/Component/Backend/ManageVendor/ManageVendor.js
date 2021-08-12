655c0a635d98e993686493b9a30965451840f602
meherinakter13 <meherinakter13@gmail.com>
2021-08-12 18:26:10 +0600

    vendor updated

--- src/Component/Backend/ManageVendor/ManageVendor.js
+++ src/Component/Backend/ManageVendor/ManageVendor.js
@@ -9,6 +9,7 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import Sidebar from '../AdminPanel/Sidebar/Sidebar';
 
 
 const StyledTableCell = withStyles((theme) => ({
@@ -100,7 +101,7 @@ export default function ManageVendor() {
   return (
     <div className="row m-0">
       <div className="col-md-2 col-lg-2 p-0">
-        {/* <Sidebar></Sidebar> */}
+        <Sidebar></Sidebar>
       </div>
       <div className="col-md-10">
         <TableContainer component={Paper}>
