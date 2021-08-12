5a9c89954150f02ee53734e8928ce8053d4c2204
meherinakter13 <meherinakter13@gmail.com>
2021-08-12 17:56:15 +0600

    gallary updated

--- src/Component/FrontEnd/Gallery/Gallery.js
+++ src/Component/FrontEnd/Gallery/Gallery.js
@@ -9,6 +9,7 @@ import Zoom from 'react-reveal/Zoom';
 const Gallery = () => {
   // const url = "https://fakestoreapi.com/products";
   const [images, setImages] = useState([]);
+  const [image, setImage] = useState([]);
 
   // const getImage = () => {
   //   axios.get(url).then((res) => {
@@ -22,7 +23,7 @@ const Gallery = () => {
   // }, []);
   useEffect(() => {
     const productsLoaders = async () => {
-        const res = await axios.get('https://sheltered-thicket-75703.herokuapp.com/products')
+        const res = await axios.get('http://localhost:5000/products')
         setImages(res.data);
     }
     productsLoaders();
@@ -30,13 +31,13 @@ const Gallery = () => {
 
 useEffect(() => {
   const productsLoaders = async () => {
-      const res = await axios.get('https://sheltered-thicket-75703.herokuapp.com/offerProducts')
-      setImages(res.data);
+      const res = await axios.get('http://localhost:5000/offerProducts')
+      setImage(res.data);
   }
   productsLoaders();
 }, []);
 
-  if (!images) {
+  if (!images&&!image) {
     return <h1>loading.....</h1>;
 
   }
@@ -45,17 +46,24 @@ useEffect(() => {
     <div className="container p-5">
       <div className="row m-0">
         {images.map((product) =>
-          <div className="col-md-6 col-sm-12 col-lg-3 d-flex justify-content-center">
+          <div className="col-md-6 col-sm-12 col-lg-3 p-3 mb-2">
             <Zoom>
-              <LazyLoadImage
-                effect="blur"
-                height="300px"
-                width="250px"
-                src={product.image}
-                key={product.id}
-                alt=""
-                className="image-hover p-3"
-              ></LazyLoadImage>
+               {
+                  product.image ? <img style={{height: "18rem", width:"15rem",border:"3px solid black"}} class="" src={`data:image/jpeg;base64,${product.image.img}`} alt="" />
+                      :
+                      <img style={{height:"300px",width:"250px",border:"3px solid black"}} className="img-fluid mb-3 " src={`https://gentle-stream-95244.herokuapp.com//${product.img}`} alt="" />
+                }
+            </Zoom>
+          </div>
+        )}
+         {image.map((products) =>
+          <div className="col-md-6 col-sm-12 col-lg-3 p-3 mb-2 ">
+            <Zoom>
+               {
+                  products.image ? <img style={{height: "18rem", width:"15rem",border:"3px solid black"}} class="" src={`data:image/jpeg;base64,${products.image.img}`} alt="" />
+                      :
+                      <img style={{height:"300px",width:"250px",border:"3px solid black"}} className="img-fluid mb-3 " src={`https://gentle-stream-95244.herokuapp.com//${products.img}`} alt="" />
+                }
             </Zoom>
           </div>
         )}
