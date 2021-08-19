29df78d9b2c7a269bf2f30d185ab891e5b2ba027
Nishat009 <nishatraihana009@gmail.com>
2021-08-19 15:06:42 +0600

    admin panel design

--- src/Component/Backend/AdminPanel/AdminDashboard/AdminDashboard.js
+++ src/Component/Backend/AdminPanel/AdminDashboard/AdminDashboard.js
@@ -1,58 +1,114 @@
-import React from 'react';
-import Sidebar from '../Sidebar/Sidebar';
+import React from "react";
+import Sidebar from "../Sidebar/Sidebar";
 
 const AdminDashboard = () => {
-    return (
-        <div className="row m-0">
-        <div className="col-md-2 col-sm-2 col-lg-2 p-0">
-       <Sidebar></Sidebar>
+  return (
+    <div className="row m-0">
+      <div className="col-md-2 col-sm-2 col-lg-2 p-0">
+        <Sidebar></Sidebar>
+      </div>
+      <div className="col-md-10 col-sm-10 mt-2 p-5">
+        <div className="row mx-auto">
+          <div className="col-md-4">
+            <div
+              style={{
+                height: "200px",
+                width: "300px",
+                backgroundColor: "#076270",
+                padding: "10px",
+                margin: "10px",
+                color: "white",
+                textAlign: "center",
+              }}
+            >
+              <p>Total Vendor</p>
+              {/* <p>{orders.length}</p> */}
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div
+              style={{
+                height: "200px",
+                width: "300px",
+                backgroundColor: "#FFC107",
+                padding: "10px",
+                margin: "10px",
+                color: "white",
+                textAlign: "center",
+              }}
+            >
+              <p>Total Customer</p>
+              {/* <p>{samples.length}</p> */}
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div
+              style={{
+                height: "200px",
+                width: "300px",
+                backgroundColor: "#076270",
+                padding: "10px",
+                margin: "10px",
+                color: "white",
+                textAlign: "center",
+              }}
+            >
+              <p>Total Category</p>
+              {/* <p>{orderSta.length}</p> */}
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div
+              style={{
+                height: "200px",
+                width: "300px",
+                backgroundColor: "#FFC107",
+                padding: "10px",
+                margin: "10px",
+                color: "white",
+                textAlign: "center",
+              }}
+            >
+              <p>Total Type</p>
+              {/* <p>{orders.length}</p> */}
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div
+              style={{
+                height: "200px",
+                width: "300px",
+                backgroundColor: "#076270",
+                padding: "10px",
+                margin: "10px",
+                color: "white",
+                textAlign: "center",
+              }}
+            >
+              <p>Total Product</p>
+              {/* <p>{samples.length}</p> */}
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div
+              style={{
+                height: "200px",
+                width: "300px",
+                backgroundColor: "#FFC107",
+                padding: "10px",
+                margin: "10px",
+                color: "white",
+                textAlign: "center",
+              }}
+            >
+              <p>Total Offer Product</p>
+              {/* <p>{orderSta.length}</p> */}
+            </div>
+          </div>
         </div>
-        <div className="col-md-10 col-sm-10 mt-2 p-5">
-      
-                <div className="row mx-auto">
-                        <div className="col-md-4">
-                        <div style={{ height: "200px", width: "300px", backgroundColor: "#076270", padding: "10px", margin: "10px", color: "white", textAlign: "center" }}>
-                            <p>Total Vendor</p>
-                            {/* <p>{orders.length}</p> */}
-                        </div>
-                        </div> 
-                         <div className="col-md-4">
-                        <div style={{ height: "200px", width: "300px", backgroundColor: "#FFC107", padding: "10px", margin: "10px", color: "white", textAlign: "center" }}>
-                            <p>Total Customer</p>
-                            {/* <p>{samples.length}</p> */}
-                        </div>
-                        </div>
-                        <div className="col-md-4">
-                        <div style={{ height: "200px", width: "300px", backgroundColor: "#076270", padding: "10px", margin: "10px", color: "white", textAlign: "center" }}>
-                            <p>Total Category</p>
-                            {/* <p>{orderSta.length}</p> */}
-                        </div>
-                        </div> 
-                        <div className="col-md-4">
-                        <div style={{ height: "200px", width: "300px", backgroundColor: "#FFC107", padding: "10px", margin: "10px", color: "white", textAlign: "center" }}>
-                            <p>Total Type</p>
-                            {/* <p>{orders.length}</p> */}
-                        </div>
-                        </div>
-                         <div className="col-md-4">
-                        <div style={{ height: "200px", width: "300px", backgroundColor: "#076270", padding: "10px", margin: "10px", color: "white", textAlign: "center" }}>
-                            <p>Total Product</p>
-                            {/* <p>{samples.length}</p> */}
-                        </div>
-                        </div>
-                        <div className="col-md-4">
-                        <div style={{ height: "200px", width: "300px", backgroundColor: "#FFC107", padding: "10px", margin: "10px", color: "white", textAlign: "center" }}>
-                            <p>Total Offer Product</p>
-                            {/* <p>{orderSta.length}</p> */}
-                        </div>
-                        
-                        </div>
-                    </div>
-                
-        </div>
- 
+      </div>
     </div>
-    );
+  );
 };
 
 export default AdminDashboard;
--- src/Component/Backend/Type/AddType/AddType.js
+++ src/Component/Backend/Type/AddType/AddType.js
@@ -36,7 +36,7 @@ const AddType = () => {
                 </div>
                 <div className="col-md-8  mt-5 col-lg-6 py-3 type  rounded">
                 <h1 className="text-center text-warning border-bottom">Add Type</h1>
-                <form class="row mx-auto  category-two  rounded container" onSubmit={handleSubmit}>
+                <form class="row mx-auto  type-two  rounded container" onSubmit={handleSubmit}>
                     <div className="col-md-12">
                         <label class="form-label fw-bolder text-white"> Type</label>
                         <input type="text" name="type" onBlur={handleBlur} class="form-control" placeholder="Enter Type Name" />
