92c40741e661bc323d507cd46a91e4f908130a5f
Nishat009 <nishatraihana009@gmail.com>
2021-09-01 12:48:11 +0600

    f

--- src/Component/FrontEnd/ProcessPayment/ProcessPayment.js
+++ src/Component/FrontEnd/ProcessPayment/ProcessPayment.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { Elements} from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import SimpleCardForm from './SimpleCardForm';
+import Navbar from '../Shared/Navbar/Navbar'
+import './ProcessPayment.css'
 const stripePromise = loadStripe('pk_test_51IeGRnC6zmfQPQheIbmBzLDvTt96GlBrhW5Iz6pLG9ndjZihu1offgwCMP4PCGlCLZ6ie7OInEmEWNLELZwpdYyE00J9LE1FzM');
 const ProcessPayment = ({handlePaymentSuccess}) => {
     return (
         <div>
+           <div>
+           <Navbar></Navbar>
+           </div>
+             <div className="payment-card">
              <Elements stripe={stripePromise}>
             <SimpleCardForm handlePaymentSuccess={handlePaymentSuccess}></SimpleCardForm>
        
         </Elements>
+             </div>
         </div>
     );
 };
--- src/Component/FrontEnd/ProcessPayment/SimpleCardForm.js
+++ src/Component/FrontEnd/ProcessPayment/SimpleCardForm.js
@@ -1,68 +1,84 @@
-import React, { useState } from 'react';
-import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
-import card from '../../../Images/credit.png'
-const SimpleCardForm = ({handlePaymentSuccess}) => {
-    const stripe = useStripe();
-    const elements = useElements();
-  
-    const [paymentError, setPaymentError] = useState(null);
-    const [paymentSuccess, setPaymentSuccess] = useState(null);
-  
-    const handleSubmit = async (event) => {
-      // Block native form submission.
-      event.preventDefault();
-  
-      if (!stripe || !elements) {
-        // Stripe.js has not loaded yet. Make sure to disable
-        // form submission until Stripe.js has loaded.
-        return;
-      }
-  
-      // Get a reference to a mounted CardElement. Elements knows how
-      // to find your CardElement because there can only ever be one of
-      // each type of element.
-      const cardElement = elements.getElement(CardElement);
-  
-      // Use your card Element with other Stripe.js APIs
-      const {error, paymentMethod} = await stripe.createPaymentMethod({
-        type: 'card',
-        card: cardElement,
-      });
-  
-      if (error) {
-        setPaymentError(error.message);
-        setPaymentSuccess(null);
-      } else {
-        // console.log('[PaymentMethod]', paymentMethod);
-        setPaymentSuccess(paymentMethod.id);
-        setPaymentError(null);
-        handlePaymentSuccess(paymentMethod.id)
-      }
-    };
-  
-    return (
-        <div>
-    <form onSubmit={handleSubmit}>
-        <div className="row m-0">
-            <div className="col-md-4">
-                <img src={card} alt="" />
+import React, { useState } from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import card from "../../../Images/credit.png";
+import "./SimpleCardForm.css";
+const SimpleCardForm = ({ handlePaymentSuccess }) => {
+  const stripe = useStripe();
+  const elements = useElements();
 
+  const [paymentError, setPaymentError] = useState(null);
+  const [paymentSuccess, setPaymentSuccess] = useState(null);
+
+  const handleSubmit = async (event) => {
+    // Block native form submission.
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return;
+    }
+
+    // Get a reference to a mounted CardElement. Elements knows how
+    // to find your CardElement because there can only ever be one of
+    // each type of element.
+    const cardElement = elements.getElement(CardElement);
+
+    // Use your card Element with other Stripe.js APIs
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: cardElement,
+    });
+
+    if (error) {
+      setPaymentError(error.message);
+      setPaymentSuccess(null);
+    } else {
+      // console.log('[PaymentMethod]', paymentMethod);
+      setPaymentSuccess(paymentMethod.id);
+      setPaymentError(null);
+      handlePaymentSuccess(paymentMethod.id);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="row m-0 pay">
+          <div className="col-md-4">
+            <div class="dropdown">
+              <img
+                src={card}
+                alt=""
+                className="image-payment dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton2"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              />
+
+              <ul
+                class="dropdown-menu dropdown-menu-light"
+                aria-labelledby="dropdownMenuButton2"
+              >
+                <CardElement className="card-element" />
+                
+                <button type="submit" disabled={!stripe}>
+                  Pay
+                </button>
+              </ul>
             </div>
-
+          </div>
         </div>
-     
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
-    {
-      paymentError && <p style={{color: 'red'}}>{paymentError}</p>
-    }
-    {
-      paymentSuccess && <p style={{color: 'green'}}>Your payment has completed successfully.</p>
-    }
-        </div>
-    );
+      </form>
+      {paymentError && <p style={{ color: "red" }}>{paymentError}</p>}
+      {paymentSuccess && (
+        <p style={{ color: "green" }}>
+          Your payment has completed successfully.
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default SimpleCardForm;
